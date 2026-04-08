d740af3950ef65fee83a8a2dc0116c3965a1e6e8
kavyan256 <iit2024245@iiita.ac.in>
2026-04-08 19:57:18 +0530

    3.1 Single Quotes

--- src/main.rs
+++ src/main.rs
@@ -18,32 +18,66 @@ fn read_command() -> String {
 //handles the command entered by the user, matches it against known commands
 //returns true if the command is "exit" to signal the main loop to break, otherwise returns false
 fn handle_command(command: &str) -> bool {
-    let parts: Vec<&str> = command.split_whitespace().collect();
+    let parts = parse_command(command);
 
     if parts.is_empty() {
         return false;
     }
 
-    match parts[0] {
+    let args: Vec<&str> = parts.iter().map(|s| s.as_str()).collect();
+
+    match args[0] {
         "exit" => {
-            if parts.len() == 1 {
+            if args.len() == 1 {
                 commands::exit()
             } else {
-                commands::cmd_not_fnd_err(parts[0])
+                commands::cmd_not_fnd_err(args[0])
             }
         }
-        "echo" => commands::echo(&parts[1..]),
+        "echo" => commands::echo(&args[1..]),
         "type" => {
-            if parts.len() > 1 {
-                commands::type_cmd(parts[1])
+            if args.len() > 1 {
+                commands::type_cmd(args[1])
             } else {
                 commands::type_cmd_err()
             }
         }
         "pwd" => commands::pwd(),
-        "cd" => commands::cd(&parts[1..]),
-        cmd => commands::run_external(cmd, &parts[1..]),
+        "cd" => commands::cd(&args[1..]),
+        cmd => commands::run_external(cmd, &args[1..]),
     }
+}
+
+fn parse_command(command: &str) -> Vec<String> {
+    let mut parts = Vec::new();
+    let mut current = String::new();
+    let mut in_quotes = false;
+
+    // Iterate through each character in the command string
+    for ch in command.chars() {
+        match ch {
+            '\'' => in_quotes = !in_quotes,
+            ' ' if !in_quotes => {
+                if !current.is_empty() {
+                    parts.push(current.clone());
+                    current.clear();
+                }
+            }
+            _ => current.push(ch),
+        }
+    }
+
+    // Check for unclosed quotes
+    if in_quotes {
+        eprintln!("parse error: unclosed quote");
+        return Vec::new();
+    }
+
+    //add the last part if it's not empty
+    if !current.is_empty() {
+        parts.push(current);
+    }
+    parts
 }
 
 fn main() {
