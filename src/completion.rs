184b4d9b01c616a857b30d44a2876108e5422851
kavyan256 <iit2024245@iiita.ac.in>
2026-04-13 11:17:42 +0530

    5.3 handling invalid completions

--- src/completion.rs
+++ src/completion.rs
@@ -20,11 +20,12 @@ impl Completer for ShellHelper {
     //takes the currently edited line with cursor position and resturns the start position and completion candidates for autocompletion
     fn complete(&self, line: &str, pos: usize, _ctx: &rustyline::Context) -> rustyline::Result<(usize, Vec<Self::Candidate>)> {
         if line == "ech" && pos == line.len() {
-            Ok((0, vec!["echo".to_string()]))
+            Ok((0, vec!["echo ".to_string()]))
         } else if line == "exi" && pos == line.len() {
             Ok((0, vec!["exit".to_string()]))
         } else {
-            Ok((0, Vec::new()))
+            print!("{}", '\x07');       //print the bell character to indicate no completions available
+            Ok((0, Vec::new()))         //empty vector for no completions 
         }
     }
 }
